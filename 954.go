709da361000a370bebbdb85108cd950d8940f114
xu.zhi <xuzhi@boohee.com>
2020-03-22 18:48:19 +0800

    增加LeetCode题954

---
+++ 954.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "sort"
+)
+
+func main(){
+    a := []int{1, 2, 2}
+    result := minIncrementForUnique(a)
+    fmt.Println("move: ", result)
+}
+
+func minIncrementForUnique(A []int) int {
+    sort.Ints(A)
+    var numbers int
+    for i, _ := range A {
+        if i <= 0 {
+            continue
+        }
+        for A[i] <= A[i-1] {
+            A[i] = A[i] + 1
+            numbers += 1
+        }
+    }
+    return numbers
+}
