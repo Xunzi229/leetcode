28e4d21ef8e1d5278256266169e631801b2c6272
xu.zhi <xuzhi@boohee.com>
2020-07-23 13:18:19 +0800

    提交 copy-list-with-random-pointer

--- leet/copy-list-with-random-pointer/138.go
+++ leet/copy-list-with-random-pointer/138.go
@@ -20,42 +20,50 @@ func copyRandomList(head *Node) *Node {
 	lists := make(map[*Node]*Node)
 
 	// 每个原Random节点对应的 存储原Random节点地址的 副本节点
-	lists2 := make(map[*Node]*Node)
+	lists2 := make(map[*Node][]*Node)
 
 	newHead := new(Node)
 	newHead = &Node{Next: head}
 
-	tmpHead := newHead
+	tmpNode := newHead
 	for head != nil {
-		tmpHead.Next = &Node{
+		tmpNode.Next = &Node{
 			Val:    head.Val,
 			Next:   head.Next,
 			Random: head.Random,
 		}
 
-		tmpHead = tmpHead.Next
+		tmpNode = tmpNode.Next
 
 		// 存这个值表示 后面 如果 有random指向这个节点的时候 ， 更改成指向这个节点的副本节点
 		// 对应的是1
-		lists[head] = tmpHead
+		lists[head] = tmpNode
 
 		// 存这个值表示， 遍历到后面的时候 ，如果后面的节点存在， 则将这个节点的Random指向后面的副本节点
 		// 此时是反向的存储。 表示到这个节点的时候需要将其指向这个节点的副本指向副本
 		// 对应的是 2
 		if head.Random != nil {
-			lists2[head.Random] = tmpHead
+			if lists2[head.Random] == nil {
+				lists2[head.Random] = make([]*Node, 0)
+			}
+			lists2[head.Random] = append(lists2[head.Random], tmpNode)
 		}
 
 		// 接下来 我们考虑的是 他有没有指向别的节点， 或者别的节点有没有指向他
 
 		// 当前节点指向前面的节点  ===> 1
 		if lists[head.Random] != nil {
-			(*tmpHead).Random = lists[head.Random]
+			tmpNode.Random = lists[head.Random]
 		}
 
 		// 前面的节点指向当前的节点 ==> 2
+		//if lists2[head] != nil {
+		//	lists2[head].Random = tmpNode
+		//}
 		if lists2[head] != nil {
-			lists2[head].Random = tmpHead
+			for _, v := range lists2[head] {
+				v.Random = tmpNode
+			}
 		}
 
 		head = head.Next
