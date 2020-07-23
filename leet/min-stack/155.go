6d7177f2af974f9d35c6d337e3d0bc2f047737b5
xu.zhi <xuzhi@boohee.com>
2020-07-23 18:50:23 +0800

    fix int

---
+++ leet/min-stack/155.go
@@ -0,0 +1,79 @@
+// https://leetcode-cn.com/problems/min-stack/
+package min_stack
+
+import (
+	"fmt"
+	"sort"
+)
+
+type MinStack struct {
+	Min     int
+	DateSet []int
+}
+
+/*
+["MinStack","push","push","push","push","getMin","pop","getMin","pop","getMin","pop","getMin"]
+[[],[2],[0],[3],[0],[],[],[],[],[],[],[]]
+
+
+[null,null,null,null,null,0,null,0,null,0,null,0]
+[null,null,null,null,null,0,null,0,null,0,null,2]
+
+
+["MinStack","push","push","top","getMin","pop","getMin","top"]
+[[],[1],[2],[],[],[],[],[]]
+
+["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+[[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+*/
+/** initialize your data structure here. */
+func Constructor() MinStack {
+	dataSet := make([]int, 0)
+	return MinStack{
+		Min:     1<<31 - 1,
+		DateSet: dataSet,
+	}
+}
+
+func (this *MinStack) Push(x int) {
+	if x <= this.Min {
+		fmt.Println("最小值:", x)
+		this.Min = x
+	}
+	this.DateSet = append(this.DateSet, x)
+}
+
+func (this *MinStack) Pop() {
+	if len(this.DateSet) == 0 {
+		return
+	}
+	this.DateSet = this.DateSet[:len(this.DateSet)-1]
+
+	var cloneDeep = make([]int, 0)
+	cloneDeep = append(cloneDeep, this.DateSet[:len(this.DateSet)]...)
+
+	fmt.Println(cloneDeep)
+	sort.Ints(cloneDeep)
+	if len(cloneDeep) != 0 {
+		this.Min = cloneDeep[0]
+	} else {
+		this.Min = 0
+	}
+}
+
+func (this *MinStack) Top() int {
+	return this.DateSet[len(this.DateSet)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.Min
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
