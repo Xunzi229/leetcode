289ca77d4573b02f497f2c7189302a7282caff10
xu.zhi <xuzhi@boohee.com>
2020-07-26 16:31:30 +0800

    柱形中最大的柱形

--- leet/largest-rectangle-in-histogram/84.go
+++ leet/largest-rectangle-in-histogram/84.go
@@ -2,7 +2,50 @@
 
 package largest_rectangle_in_histogram
 
-//
+// TODO next
+func largestRectangleArea(heights []int) int {
+	lenHeight := len(heights)
+	if lenHeight == 0 {
+		return 0
+	}
+	stack := make([]int, 0)
+	max := 0
+	for i := 0; i <= len(heights); i++ {
+		var cur int
+		if i == lenHeight {
+			cur = 0
+		} else {
+			cur = heights[i]
+		}
+		// 当前高度小于栈，则将栈内元素都弹出计算面积
+		for len(stack) != 0 && cur <= heights[stack[len(stack)-1]] {
+			pop := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			h := heights[pop]
+
+			// 计算宽度
+			w := i
+			if len(stack) != 0 {
+				peek := stack[len(stack)-1]
+				w = i - peek - 1
+			}
+			max = Max(max, h*w)
+		}
+		// 记录索引即可获取对应元素
+		stack = append(stack, i)
+	}
+	return max
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 该方法虽然执行的速度上有所提升并且通过测试但是还未是效率上最好的
+/*
 func largestRectangleArea(heights []int) int {
 	max := 0
 	lenHeight := len(heights)
@@ -26,9 +69,16 @@ func largestRectangleAreaDeep(heights []int, currentIndex, rightIndex, lenHeight
 		if left > *(max) {
 			*(max) = left
 		}
+		if heights[rightIndex] < heights[currentIndex] {
+			if heights[rightIndex]+lenHeight-currentIndex+1 < *max {
+				return
+			}
+		}
+
 		rightIndex++
 	}
 }
+*/
 
 /*
 使用递归， 超出时间限制， 所以这个方式并不合适
