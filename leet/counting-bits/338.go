fe07a87ce477641661e7349b3ffb1bc477bb37df
xu.zhi <xuzhi@boohee.com>
2020-07-30 14:29:47 +0800

    统计1出现次数

--- leet/counting-bits/338.go
+++ leet/counting-bits/338.go
@@ -3,5 +3,22 @@
 package counting_bits
 
 func countBits(num int) []int {
+	n := make([]int, num+1)
 
+	for i := 0; i <= num; i++ {
+		n[i] = count(i)
+	}
+	return n
 }
+
+func count(num int) int {
+	var res int
+	for num != 0 {
+		num &= num - 1
+		res++
+	}
+	return res
+}
+
+// 动态规划的解法
+// TODO
