611797717e335ac889ae24aa9e55a4d788911b19
xu.zhi <xuzhi@boohee.com>
2020-08-01 09:46:31 +0800

    寻找旋转排序数组中的最小值 II

--- 二分搜索/find-minimum-in-rotated-sorted-array/153.go
+++ 二分搜索/find-minimum-in-rotated-sorted-array/153.go
@@ -2,30 +2,23 @@
 package find_minimum_in_rotated_sorted_array
 
 func findMin(nums []int) int {
-	var start, end = 0, len(nums) - 1
+	if len(nums) == 0 {
+        return -1
+    }
 
-	tmpStart := nums[0]
-	for start+1 < end {
-		mid := start + (end-start)/2
+    start := 0
+    end := len(nums) - 1
 
-		if nums[mid] > nums[mid+1] {
-			if nums[mid+1] < tmpStart {
-				return nums[mid+1]
-			}
-			return tmpStart
-		}
-		if mid-1 >= 0 {
-			left := findMin(nums[start : mid-1])
-			right := findMin(nums[mid+1 : end])
-			if left > right {
-				return right
-			}
-			return left
-		}
-		return findMin(nums[mid+1 : end])
-	}
-	if nums[start] > nums[end] {
-		return nums[end]
-	}
-	return nums[start]
+    for start+1 < end {
+        mid := start + (end-start)/2
+        if nums[mid] <= nums[end] {
+            end = mid
+        } else {
+            start = mid
+        }
+    }
+    if nums[start] > nums[end] {
+        return nums[end]
+    }
+    return nums[start]
 }
