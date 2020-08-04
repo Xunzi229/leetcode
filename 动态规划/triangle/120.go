91bd9794ef1d45701f974e397a62e5e47103e147
xu.zhi <xuzhi@boohee.com>
2020-08-04 22:25:28 +0800

    动态规划自上而下

--- 动态规划/triangle/120.go
+++ 动态规划/triangle/120.go
@@ -3,10 +3,14 @@ package triangle
 
 // 动态规划自底向上
 func minimumTotal(triangle [][]int) int {
+	/*
+		// DFS 方式
+		return minimumTotalDFS(triangle)
+	*/
+
 	if len(triangle) == 0 || len(triangle[0]) == 0 {
 		return 0
 	}
-
 	// 1 定一个一个函数表示每一层的位置 function[i][j]
 	// 初始化
 	c := len(triangle)
@@ -41,7 +45,6 @@ func min(a, b int) int {
 }
 
 /*
-
 // 使用 DFS, 记忆话搜索, 这里已经是用到了动态规划
 func minimumTotalDFS(triangle [][]int) int {
 	c := len(triangle)
@@ -75,3 +78,36 @@ func minimumTotalDFSDeep(nums, triangle [][]int, x, y int) int {
 	return nums[x][y]
 }
 */
+
+// 自上而下
+func minimumTotalW(triangle [][]int) int {
+	c := len(triangle)
+	nums := make([][]int, c)
+	// 2、初始化
+	for i := 0; i < c; i++ {
+		for j := 0; j < len(triangle[i]); j++ {
+			if nums[i] == nil {
+				nums[i] = make([]int, len(triangle[i]))
+			}
+			nums[i][j] = triangle[i][j]
+		}
+	}
+
+	for i := 1; i < c; i++ {
+		for j := 0; j < len(triangle[i]); j++ {
+			if j-1 < 0 {
+				nums[i][j] = nums[i-1][j] + triangle[i][j]
+			} else if j >= len(nums[i-1]) {
+				nums[i][j] = nums[i-1][j-1] + triangle[i][j]
+			} else {
+				nums[i][j] = min(nums[i-1][j], nums[i-1][j-1]) + triangle[i][j]
+			}
+		}
+	}
+
+	result := nums[c-1][0]
+	for i := 1; i < len(nums[c-1]); i++ {
+		result = min(result, nums[c-1][i])
+	}
+	return result
+}
