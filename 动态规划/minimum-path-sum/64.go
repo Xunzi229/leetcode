41072def896132caf687a678f544463852d95e01
xu.zhi <xuzhi@boohee.com>
2020-08-05 21:29:24 +0800

    动态规划-最小路径和

--- 动态规划/minimum-path-sum/64.go
+++ 动态规划/minimum-path-sum/64.go
@@ -4,30 +4,39 @@ package minimum_path_sum
 import "fmt"
 
 func minPathSum(grid [][]int) int {
-	//c := len(grid)
-	//nums := make([][]int, c)
-	//// 2、初始化
-	//for i := 0; i < c; i++ {
-	//	for j := 0; j < len(grid[i]); j++ {
-	//		if nums[i] == nil {
-	//			nums[i] = make([]int, len(grid[i]))
-	//		}
-	//		nums[i][j] = -1
-	//	}
-	//}
-	//var result int
-	_ok, result := minPathSumDeep(grid, 0, 0)
-	fmt.Println(_ok)
-	return result
-}
+	c := len(grid)
+	dp := make([][]int, c)
+	// 2、初始化
+	for i := 0; i < c; i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if dp[i] == nil {
+				dp[i] = make([]int, len(grid[i]))
+			}
+			dp[i][j] = grid[i][j]
+		}
+	}
 
-// 右上角 到 右下角的位置
-func minPathSumDeep(grid [][]int, x, y int) (bool, int) {
+	// 动态规划的过程
+	for i := 0; i <= len(grid)-1; i++ {
+		for j := 0; j <= len(grid[i])-1; j++ {
+			if i != 0 && j != 0 {
+				dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+			}
+			if i != 0 && j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			}
+			if i == 0 && j != 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			}
+		}
+	}
+	fmt.Println(len(dp[len(grid)-1]))
+	return dp[len(grid)-1][len(dp[len(grid)-1])-1]
 }
 
 func min(a, b int) int {
-	if a > b {
-		return b
+	if a < b {
+		return a
 	}
-	return a
+	return b
 }
