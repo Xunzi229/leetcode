93ce4a47945bf8edc634c7964c98c67e03443a6a
xu.zhi <xuzhi@boohee.com>
2020-08-06 15:58:21 +0800

    动态规划-跳跃游戏 II

--- 动态规划/jump-game-ii/45.go
+++ 动态规划/jump-game-ii/45.go
@@ -32,3 +32,27 @@ func jump(nums []int) int {
 	}
 	return 0
 }
+
+// 如何使用动态规划
+// 明确的是 某个点 能最少步数到达 , 那就是 依次每个点都是最少步数到达
+func jumpDynamic(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = 0
+
+	for i := 1; i < len(nums); i++ {
+		dp[i] = i
+		for j := 0; j < i; j++ {
+			if nums[j]+j >= i {
+				dp[i] = min(dp[j]+1, dp[i])
+			}
+		}
+	}
+	return dp[len(nums)-1]
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
