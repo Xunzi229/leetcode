a226d1ae3a1b55ad12fae4a1a1984d5a5e58e5ca
xu.zhi <xuzhi@boohee.com>
2020-08-08 20:03:54 +0800

    动态规划-最长公共子序列

---
+++ 动态规划/longest-common-subsequence/1143.go
@@ -0,0 +1,43 @@
+/*
+https://leetcode-cn.com/problems/longest-common-subsequence/
+*/
+package longest_common_subsequence
+
+/*
+"abcde"
+"ace"
+
+"oxcpqrsvwf"
+"shmtulqrypy"
+
+"bl"
+"xbl"
+
+"bl"
+"yby"
+
+"ezupkr"
+"ubmrapg"
+*/
+func longestCommonSubsequence(a string, b string) int {
+	dp := make([][]int, len(a)+1)
+	for i := 0; i <= len(a); i++ {
+		dp[i] = make([]int, len(b)+1)
+	}
+	for i := 1; i <= len(a); i++ {
+		for j := 1; j <= len(b); j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[len(a)][len(b)]
+}
+func max(a,b int)int {
+    if a>b{
+        return a
+    }
+    return b
+}
