83ceef95879cc73f45a9c32bfcf45d7cbc891bcc
xu.zhi <xuzhi@boohee.com>
2020-08-11 14:23:32 +0800

    动态规划-解码方法

--- 动态规划/decode-ways/91.go
+++ 动态规划/decode-ways/91.go
@@ -5,12 +5,19 @@ package decode_ways
 
 // 0 => 48
 // TODO
+// 状态转移  dp[i] = dp[i-1] + dp[i-2]
+// 考虑数值 0 在其中的 作用
+// 如果 dp[i-1] 和 dp[i-2] 都是为
+// 如果 dp[i-1] = 0 , 则 dp[i-2]
+// 如果 dp[i-2] = 0 则 dp[i-1]
+
 func numDecodings(s string) int {
 	dp := make([]int, len(s)+1)
-	dp[0] = 1
+	dp[0] = 0
 	if s[0] != 48 {
 		dp[1] = 1
 	}
+
 	if len(s) <= 1 {
 		return dp[1]
 	}
@@ -33,4 +40,40 @@ func numDecodings(s string) int {
 		}
 	}
 	return dp[len(dp)-1]
+}
+
+// 少存储点空间, 只使用前后变量变化
+func numDecodingsWithOutSpace(s string) int {
+	var cur, preStep1, preStep2 = 0, 0, 0
+	cur = 0
+	if s[0] != 48 {
+		cur = 1
+	}
+
+	if len(s) <= 1 {
+		return cur
+	}
+	preStep1, preStep2 = cur, cur
+
+	for i := 2; i <= len(s); i++ {
+		if s[i-2] == 48 && s[i-1] == 48 {
+			return 0
+		}
+		num := (s[i-2]-48)*10 + (s[i-1] - 48)
+		// "1011"
+		if s[i-2] == 48 || num > 26 {
+			cur = preStep1
+		} else if s[i-1] == 48 {
+			// "501"
+			if num > 26 {
+				return 0
+			}
+			cur = preStep2
+		} else {
+			// "111"
+			cur = preStep1 + preStep2
+		}
+		preStep1, preStep2 = cur, preStep1
+	}
+	return cur
 }
