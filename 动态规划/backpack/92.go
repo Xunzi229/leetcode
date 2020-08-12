2b38e8ad0bbe375fdfe39ec5beac9f1f46b0416a
xu.zhi <xuzhi@boohee.com>
2020-08-12 23:06:00 +0800

    动态规划-背包问题

--- 动态规划/backpack/92.go
+++ 动态规划/backpack/92.go
@@ -30,26 +30,36 @@ O(n x m) 的时间复杂度 and O(m) 空间复杂度
  * @param A: Given n items with size A[i]
  * @return: The maximum size
  */
+
+// 如何思考, 思考的目的是什么,
+// 最终想要的结果是 我能不能将当前的物品继续向包里面装
+// 如何选择最优的解决方案
+// 物品的个数是有限的(放完是不会放回去的), 包的重量是已知的
+
+// 反思维: 包在装物品的同时, 也就是说这个物品的重量里面有这个包, 譬如 我物品重量是 5, 那么包是 5以后的都能装下这个包
+
 func backPack(m int, A []int) int {
-	// 假设包的大小是变化的, 那么这个问题就是 就变成递归了
+	dp := make([][]bool, len(A)+1) // + 1 长度上是 0, 0
+	for i := 0; i <= len(A); i++ {
+		dp[i] = make([]bool, m+1) // + 1 长度上是 0, 0
+	}
+	dp[0][0] = true
 
-	dp := make([]int, m+1)
-	dp[0] = 0 // 当包的大小为 0的时候是不能存放任何东西的
-
-	for i := 1; i <= m; i++ {
-		for j := 0; j <= len(A)-1; j++ {
-			// 保证包不会超重
-			if i-A[j] >= 0 {
-				dp[i] = max(dp[i], dp[i-A[j]]+1)
+	for i := 1; i <= len(A); i++ {
+		for j := 0; j <= m; j++ {
+			dp[i][j] = dp[i-1][j] //
+			// j-A[i-1] >= 0 表示加上 j 会不会超重
+			// dp[i-1][j-A[i-1]] // 表示这个是否是个可选的重量, 因为涉及是状态的变更, 我们不关心走到在哪一步, 我们只关心状态变更
+			if j-A[i-1] >= 0 && dp[i-1][j-A[i-1]] {
+				dp[i][j] = true
 			}
 		}
 	}
-	return dp[m]
-}
 
-func max(a, b int) int {
-	if a > b {
-		return a
+	for i := m; i >= 0; i-- {
+		if dp[len(A)][i] {
+			return i
+		}
 	}
-	return b
+	return 0
 }
