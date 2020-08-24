f004a53ceeaa9a4224efb1f063d5ff52b6abbd1c
xu.zhi <xuzhi@boohee.com>
2020-08-24 10:54:05 +0800

    计算超时唉

--- 算法思想篇/滑动窗口思想/minimum-window-substring/76.go
+++ 算法思想篇/滑动窗口思想/minimum-window-substring/76.go
@@ -3,56 +3,88 @@ https://leetcode-cn.com/problems/minimum-window-substring/submissions/
 */
 package minimum_window_substring
 
+/*
 func minWindow(s string, t string) string {
-    left, right := 0, 0
-    windows := make([]string, 0)
+	ori, cnt := map[byte]int{}, map[byte]int{}
+	for i := 0; i < len(t); i++ {
+		ori[t[i]]++
+	}
 
-    minStr := s
-    for right < len(s) {
-        windows = append(windows, string(s[right]))
-        right++
-        for valid(windows, t) && left < right {
-            str := func() string {
-                f := ""
-                for _, v := range windows {
-                    f += v
-                }
-                return f
-            }()
-            if len(str) < len(minStr) {
-                minStr = str
-            }
+	sLen := len(s)
+	len := math.MaxInt32
+	ansL, ansR := -1, -1
 
-            windows = append([]string{}, windows[1:]...)
-            left++
-        }
-    }
-    return minStr
+	check := func() bool {
+		for k, v := range ori {
+			if cnt[k] < v {
+				return false
+			}
+		}
+		return true
+	}
+	for l, r := 0, 0; r < sLen; r++ {
+		if r < sLen && ori[s[r]] > 0 {
+			cnt[s[r]]++
+		}
+		for check() && l <= r {
+			if r-l+1 < len {
+				len = r - l + 1
+				ansL, ansR = l, l+len
+			}
+			if _, ok := ori[s[l]]; ok {
+				cnt[s[l]] -= 1
+			}
+			l++
+		}
+	}
+	if ansL == -1 {
+		return ""
+	}
+	return s[ansL:ansR]
 }
+*/
 
-func valid(windows []string, t string) bool {
-    for {
-        for _, v := range t {
-            vStr := string(v)
+// 此种方式计算会超时
+func minWindow(s string, t string) string {
+	left, right := 0, 0
+	windows := make([]int, 0)
 
-            c := false
-            for i, v := range windows {
-                if v == vStr {
-                    c = true
-                    if i == 0 {
-                         windows = append([]string{}, windows[1:]...)
-                    } else if i == len(windows) - 1 {
-                        windows = append([]string{}, windows[0:(len(windows) - 1)]...)
-                    } else {
-                        windows = append(windows[0:i], windows[(i+1):]...)
-                    }
-                    break
-                }
-            }
-            if !c {
-                return false
-            }
-        }
-        return true
-    }
+	tMap := map[byte]int{}
+	for i, _ := range t {
+		tMap[t[i]] += 1
+	}
+	wMap := map[byte]int{}
+
+	validMap := func() bool {
+		for kt, vt := range tMap {
+			if wMap[kt] < vt {
+				return false
+			}
+		}
+		return true
+	}
+	minStr := s + t
+	for right < len(s) {
+		windows = append(windows, right)
+		wMap[s[right]] += 1
+		right++
+
+		for validMap() && left < right {
+			str := ""
+			for _, v := range windows {
+				str += string(s[v])
+			}
+
+			if len(str) < len(minStr) {
+				minStr = str
+			}
+			wMap[s[(windows[0])]] -= 1
+			windows = windows[1:]
+			left++
+		}
+	}
+	if len(minStr) > len(s) {
+		return ""
+	}
+	return minStr
 }
