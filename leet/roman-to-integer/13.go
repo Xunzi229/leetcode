7469c61f4673d08fd20e7e87053950b085de9a88
xunzi229 <gogogk@126.com>
2021-03-25 23:19:18 +0800

    罗马数字转整数

---
+++ leet/roman-to-integer/13.go
@@ -0,0 +1,35 @@
+/*
+https://leetcode-cn.com/problems/roman-to-integer/submissions/
+*/
+
+package roman_to_integer
+
+func romanToInt(s string) int {
+	ref := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	if len(s) == 1 {
+		return ref[[]byte(s)[0]]
+	}
+
+	total := 0
+	for i, _ := range s {
+		if len(s) <= i+1 {
+			total += ref[s[i]]
+			return total
+		}
+		if ref[s[i]] < ref[s[i+1]] {
+			total -= ref[s[i]]
+			continue
+		}
+		total += ref[s[i]]
+	}
+	return 0
+}
---
+++ leet/roman-to-integer/13_test.go
@@ -0,0 +1,10 @@
+package roman_to_integer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRomanToInt(t *testing.T) {
+	fmt.Println(romanToInt("XXVII"))
+}
