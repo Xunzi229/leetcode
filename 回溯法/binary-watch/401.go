70e428bf29949d8e44cbb43032780ad91192a6d6
xunzi229 <gogogk@126.com>
2021-04-01 00:16:42 +0800

    二进制手表

--- 回溯法/binary-watch/401.go
+++ 回溯法/binary-watch/401.go
@@ -13,49 +13,55 @@ import (
 */
 // TODO
 func readBinaryWatch(num int) []string {
-	watch := make([]string, 0)
+	watch := make(map[string]string)
 
 	var backtrace func(ps, num int)
 
 	// 前四位表示的上面的4个 LED
 	// 后6个表示的是底部的 LED
-	chess := make([]int, 10)
+	chess := make([]int, 10) // 表示的是当前的一个状态
 	// num 表示剩余, c 表示当前的位置
 	backtrace = func(c, n int) {
-		if n == 0 || c == 10 {
+		if n == 0 || c == 10 { // 当前位置超过了， 或者 已经没有可以
 			if v, ok := calc(chess); ok {
-				watch = append(watch, v)
+				watch[v] = v
 			}
+			return
 		}
-		for i := c; i < c+n; i++ {
-			chess[i] = 1
-			backtrace(i+1, n-1)
-			chess[i] = 0
+
+		for n != 0 && c < 10 {
+			chess[c] = 1
+			backtrace(c+1, n-1)
+			chess[c] = 0
+			c++
 		}
-	}
-	for i := 0; i < len(chess); i++ {
-		backtrace(i, num)
 	}
 
-	return watch
+	for i := 0; i <= 0; i++ {
+		if i+num <= 9 {
+			backtrace(i, num)
+		}
+	}
+	r := make([]string, 0)
+	for _, v := range watch {
+		r = append(r, v)
+	}
+	return r
 }
 
 // 判断最终值
 func calc(chess []int) (r string, ok bool) {
-	if len(chess) != 10 {
-		return "", false
-	}
 	h := 0
 	m := 0
 	for i := 0; i < len(chess); i++ {
 		if i < 4 {
-			h = h + int(math.Pow(2, float64(i)))*chess[i]
+			h += int(math.Pow(2, float64(i))) * chess[i]
 			if h > 11 {
 				return "", false
 			}
 		} else {
-			m = m + int(math.Pow(2, float64(i-4)))*chess[i]
-			if h > 59 {
+			m += int(math.Pow(2, float64(i-4))) * chess[i]
+			if m > 59 {
 				return "", false
 			}
 		}
