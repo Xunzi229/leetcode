24f9d0eba827e5e2e7a6843ab96fa9268cf79f10
xunzi229 <gogogk@126.com>
2021-04-14 23:55:34 +0800

    增加980. 不同路径 III, 优化性能双百过关

--- traceback/unique-paths-iii/980.go
+++ traceback/unique-paths-iii/980.go
@@ -19,38 +19,34 @@ func uniquePathsIII(grid [][]int) int {
 	// 向下 x++
 	// 向左 y--
 	// 向右 y++
-	start := []int{0, 0}
-	end := []int{0, 0}
+	x0, y0 := 0, 0
+	x1, y1 := 0, 0
 	needStep := 0
 
-	var repeatGrid = make([][]int, len(grid))
 	for x := 0; x < len(grid); x++ {
-		repeatGrid[x] = make([]int, len(grid[x]))
 		for y := 0; y < len(grid[x]); y++ {
 			if grid[x][y] == 1 {
-				start = []int{x, y}
-				repeatGrid[x][y] = -1
+				x0 = x
+				y0 = y
+				grid[x][y] = -1
 			}
 			if grid[x][y] == 2 {
-				end = []int{x, y}
+				x1 = x
+				y1 = y
 			}
 			if grid[x][y] == 0 {
 				needStep++
-			}
-
-			if grid[x][y] == -1 {
-				repeatGrid[x][y] = -1
 			}
 		}
 	}
 
 	var res int
 
-	var backtrace func(x, y, step int, colorGrid *[][]int)
+	var backtrace func(x, y, step int)
 
-	backtrace = func(x, y, step int, colorGrid *[][]int) {
+	backtrace = func(x, y, step int) {
 		// 到达终点的标志, 为啥等于 -1, 因为 0 的表示都走完了，最后一步到这个地方就变成-1了
-		if x == end[0] && y == end[1] {
+		if x == x1 && y == y1 {
 			if step == -1 {
 				res++
 			}
@@ -62,46 +58,53 @@ func uniquePathsIII(grid [][]int) int {
 
 		for {
 			// 向上
-			if x-1 >= 0 && grid[x-1][y] != -1 && repeatGrid[x-1][y] != -1 {
-				repeatGrid[x-1][y] = -1
-				step--
-				backtrace(x-1, y, step, &repeatGrid)
-				step++
-				repeatGrid[x-1][y] = 0
+			if x-1 >= 0 && grid[x-1][y] != -1 {
+				if !(step != 0 && grid[x-1][y] == 2) {
+					grid[x-1][y] = -1
+					step--
+					backtrace(x-1, y, step)
+					step++
+					grid[x-1][y] = 0
+				}
 			}
 
 			// 向下
-			if x+1 < len(grid) && grid[x+1][y] != -1 && repeatGrid[x+1][y] != -1 {
-				repeatGrid[x+1][y] = -1
-				step--
-				backtrace(x+1, y, step, &repeatGrid)
-				step++
-				repeatGrid[x+1][y] = 0
+			if x+1 < len(grid) && grid[x+1][y] != -1 {
+				if !(step != 0 && grid[x+1][y] == 2) {
+					grid[x+1][y] = -1
+					step--
+					backtrace(x+1, y, step)
+					step++
+					grid[x+1][y] = 0
+				}
 			}
 
 			// 向左
-			if y-1 >= 0 && grid[x][y-1] != -1 && repeatGrid[x][y-1] != -1 {
-				repeatGrid[x][y-1] = -1
-				step--
-				backtrace(x, y-1, step, &repeatGrid)
-				step++
-				repeatGrid[x][y-1] = 0
+			if y-1 >= 0 && grid[x][y-1] != -1 {
+				if !(step != 0 && grid[x][y-1] == 2) {
+					grid[x][y-1] = -1
+					step--
+					backtrace(x, y-1, step)
+					step++
+					grid[x][y-1] = 0
+				}
 			}
 
 			// 向右
-			if y+1 < len(grid[x]) && grid[x][y+1] != -1 && repeatGrid[x][y+1] != -1 {
-				repeatGrid[x][y+1] = -1
-				step--
-				backtrace(x, y+1, step, &repeatGrid)
-				step++
-				repeatGrid[x][y+1] = 0
+			if y+1 < len(grid[x]) && grid[x][y+1] != -1 {
+				if !(step != 0 && grid[x][y+1] == 2) {
+					grid[x][y+1] = -1
+					step--
+					backtrace(x, y+1, step)
+					step++
+					grid[x][y+1] = 0
+				}
 			}
-
 			return
 		}
 	}
 
-	backtrace(start[0], start[1], needStep, &repeatGrid)
+	backtrace(x0, y0, needStep)
 
 	return res
 }
