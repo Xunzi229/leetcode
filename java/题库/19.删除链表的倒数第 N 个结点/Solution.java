d5b223b0f4307e602ddedbda49e85822c81c0e85
xunzi229 <gogogk@126.com>
2023-04-03 21:43:15 +0800

    load data

--- java/题库/19.删除链表的倒数第 N 个结点/Solution.java
+++ java/题库/19.删除链表的倒数第 N 个结点/Solution.java
@@ -1,45 +1,48 @@
+import java.util.ArrayList;
+
 /*
 19. 删除链表的倒数第 N 个结点
 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
 https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
 */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        List<ListNode> list = new ArrayList<>();
 
-public ListNode removeNthFromEnd(ListNode head, int n) {
-    List<ListNode> list = new ArrayList<>();
-
-    while (head.next != null) {
-        list.add(head);
-        head = head.next;
-    }
-    int nx = list.size() - n;
-
-    System.out.println(nx);
-    ListNode newHead = new ListNode();
-
-    ListNode currentHead = newHead;
-    for (int index = 0; index < list.size(); index++) {
-        if (index == nx) {
-            continue;
+        while (head.next != null) {
+            list.add(head);
+            head = head.next;
         }
-        currentHead.next = list.get(index);
-        list.get(index).next = null;
-        currentHead = currentHead.next;
-    }
-    return newHead.next;
-}
+        int nx = list.size() - n;
 
-public ListNode removeNthFromEnd1(ListNode head, int n) {
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    ListNode fast = dummy;
-    ListNode slow = dummy;
-    for (int i = 0; i <= n; i++) {
-        fast = fast.next;
+        System.out.println(nx);
+        ListNode newHead = new ListNode();
+
+        ListNode currentHead = newHead;
+        for (int index = 0; index < list.size(); index++) {
+            if (index == nx) {
+                continue;
+            }
+            currentHead.next = list.get(index);
+            list.get(index).next = null;
+            currentHead = currentHead.next;
+        }
+        return newHead.next;
     }
-    while (fast != null) {
-        fast = fast.next;
-        slow = slow.next;
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
     }
-    slow.next = slow.next.next;
-    return dummy.next;
 }
